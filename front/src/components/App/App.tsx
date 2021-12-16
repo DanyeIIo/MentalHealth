c9beaf51974797619c3c8aefa4498e2a51fcadfe
Hrant <grishyan.hrant@gmail.com>
2021-12-16 17:06:32 +0400

    main page

--- front/src/components/App/App.tsx
+++ front/src/components/App/App.tsx
@@ -3,14 +3,16 @@ import About from '../about/About'
 import Services from "../services/Services";
 import Authorization from "../authorization/Authorization";
 import Registration from "../registration/Registration";
+import MainPage from "../mainPage/MainPage";
 
 const App = () => {
     return (<>
         <Header />
         {/* <About /> */}
-        <Services />
+        {/* <Services /> */}
         {/* <Authorization /> */}
         {/* <Registration /> */}
+        <MainPage />
     </>)
 }
 
